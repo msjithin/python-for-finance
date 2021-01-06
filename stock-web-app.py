221486374b32c239ee8b639a1adb724cdc40e6a5
Jithin Madhusudanan Sreekala <ms@wisc.edu>
2021-01-06 02:55:07 +0100

    Adds plotpy Candlestick chart

--- stock-web-app.py
+++ stock-web-app.py
@@ -8,14 +8,17 @@ import streamlit as st
 import datetime as dt
 import matplotlib.pyplot as plt
 from matplotlib import style
+from matplotlib.dates import date2num
 import pandas as pd
 import pandas_datareader.data as web
 from pathlib import Path
 import json
-#import plotly.express as px
+import plotly.express as px
+import plotly.graph_objects as go
+
 from streamlit.elements import button
 # Use the full page instead of a narrow central column
-st.set_page_config(layout="wide")
+st.set_page_config(page_title='Stock web app', layout="wide")
 
 image1, image2, image3 = st.beta_columns([2,1,1])
 #Add title and image 
@@ -39,7 +42,8 @@ def get_csv(stock_name = 'stock', start_year=2000, end_year=2020):
        end_year   : int
     '''
     start = dt.datetime(start_year, 1, 1)
-    end = dt.datetime(end_year, 12, 31)
+    #end = dt.datetime(end_year, 12, 31)
+    end = dt.date.today()
     df = web.DataReader(str(stock_name), 'yahoo', start, end)
     df.to_csv('data/'+stock_name.lower()+'.csv')
 
@@ -57,12 +61,12 @@ def get_company_name(symbol):
         data = json.load(jsfile)
         for p in data:
             if symbol.upper() == p['Symbol']:
-                return p['Company Name']
+                return p['Company Name'], 'NASDAQ'
     with open('data/nyse-listed_json.json') as jsfile:
         data = json.load(jsfile)
         for p in data:
             if symbol.upper() == p['ACT Symbol']:
-                return p['Company Name']
+                return p['Company Name'], 'NYSE'
     return symbol
 
 #Create function to get dataset
@@ -103,7 +107,7 @@ start, end, symbol = get_input()
 #get data
 df = get_data(symbol, start, end)
 #get company name
-company_name = get_company_name(symbol)
+company_name, listed_at = get_company_name(symbol)
 company = st.sidebar.text_input("Company", company_name)
 # For radio box selection - select time range using radio box
 # st.sidebar.subheader('Select Date range')
@@ -133,6 +137,7 @@ b4 = button4.button('Last 3 months')
 
 #Display the close price
 st.header(company_name+' Close price \n')
+st.subheader(listed_at + ': '+symbol.upper())
 if b1:
     st.line_chart(df['Close'])
 elif b4:
@@ -145,17 +150,19 @@ elif b2:
     df = df.last('5Y')
     st.line_chart(df['Close'])
 else:
+    p = plt.axvspan(0, 0.5, facecolor='y', alpha=0.5,zorder=3)
     st.line_chart(df['Close'])
 
+c1, c2, c3 = st.beta_columns([1,2,1])
 #add select widget
 st.sidebar.subheader('Select plot for second chart range')
 x_options = ['Date','High', 'Low', 'Open', 'Close', 'Volume', 'Adj Close']
 y_options = ['Volume', 'High', 'Low', 'Open', 'Close', 'Adj Close']
 dropdown_select1 = st.sidebar.selectbox(label='X axis', options=x_options)
 dropdown_select2 = st.sidebar.selectbox(label='Y axis', options=y_options)
-st.header(company_name+' '+dropdown_select2+'\n')
+c2.header(company_name+' '+dropdown_select2+'\n')
 if dropdown_select1 == 'Date':
-    st.line_chart(df[dropdown_select2])
+    c2.line_chart(df[dropdown_select2])
 else:
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
@@ -165,7 +172,7 @@ else:
     )
     ax.set_xlabel(dropdown_select1)
     ax.set_ylabel(dropdown_select2)
-    st.write(fig)
+    c2.write(fig)
 
 # checkbox widget
 checkbox_stat = st.sidebar.checkbox('Reveal data statistics')
@@ -180,6 +187,59 @@ if checkbox:
     st.header('Data')
     #st.write(df)
     st.dataframe(data=df)
+
+
+
+
+fig = px.line(df['Close'], width=1150, height=500)
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+st.write(fig)
+
+
+
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['Open'],
+                high=df['High'],
+                low=df['Low'],
+                close=df['Close'])])
+fig.update_xaxes(
+    rangeslider_visible=True,
+    rangeselector=dict(
+        buttons=list([
+            dict(count=1, label="1m", step="month", stepmode="backward"),
+            dict(count=6, label="6m", step="month", stepmode="backward"),
+            dict(count=1, label="YTD", step="year", stepmode="todate"),
+            dict(count=1, label="1y", step="year", stepmode="backward"),
+            dict(step="all")
+        ])
+    )
+)
+fig.update_layout(
+    title='Candlestick graph',
+    yaxis_title='AAPL Stock',
+    width=1150,
+    height=500,
+    shapes = [dict(
+        x0='2016-12-09', x1='2016-12-09', y0=0, y1=1, xref='x', yref='paper',
+        line_width=2)],
+    annotations=[dict(
+        x='2016-12-09', y=0.05, xref='x', yref='paper',
+        showarrow=False, xanchor='left', text='Increase Period Begins')]
+)
+st.write(fig)
+
+
 
 
 print('Everything ran ..... ')
