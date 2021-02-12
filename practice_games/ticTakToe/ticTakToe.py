7788181381c432701356a78b64ffe95eb92d94e2
Jithin Madhusudanan Sreekala <ms@wisc.edu>
2021-02-12 11:07:50 +0100

    adds message box for winner

--- practice_games/ticTakToe/ticTakToe.py
+++ practice_games/ticTakToe/ticTakToe.py
@@ -1,5 +1,7 @@
 from tkinter import *
-import random 
+from tkinter import messagebox 
+import random
+import tkinter 
 from PIL import ImageTk,Image  
 
 root = Tk()
@@ -9,53 +11,87 @@ canv = Canvas(root, width=420, height=420, bd=0)
 canv.pack() 
 STEP = 115
 nClick = 0
-
+reset = False
+backkground = PhotoImage(file ="assets/background.png")
+x_Photo = ImageTk.PhotoImage(Image.open("assets/x_2.png"))
+o_Photo = ImageTk.PhotoImage(Image.open("assets/o_2.png"))
+square_photo = ImageTk.PhotoImage(Image.open("assets/square.png"))
+x_clicked = [ 0 for i in range(9) ]
+o_clicked = [ 0 for i in range(9) ]
+canv.create_image(210,210, image=backkground)      
+buttons = []
+B00 = ''
+B01 = ''
+B02 = ''
+B10 = ''
+B11 = ''
+B12 = '' 
+B20 = ''
+B21 = ''
+B22 = ''
 def player_x():
     pass
 
 def player_o():
     pass
+def check_score(clicks):
+    if sum(clicks) > 2:
+        for i in [0, 3, 6]:
+            if clicks[i] + clicks[i+1] + clicks[i+2] == 3:
+                return True
+        for i in [0, 1, 2]:
+            if clicks[i] + clicks[i+3] + clicks[i+6] == 3:
+                return True
+        if clicks[0] + clicks[4] + clicks[8] == 3:
+            return True
+        if clicks[2] + clicks[4] + clicks[6] == 3:
+            return True
+    return False
 
-backkground = PhotoImage(file ="assets/background.png")
-#canv.create_image(0, 0, image=backkground)
-
-# x_Photo = PhotoImage(file = "assets/x_2.png")
-# o_Photo = PhotoImage(file = "assets/o_2.png")
-x_Photo = ImageTk.PhotoImage(Image.open("assets/x_2.png"))
-o_Photo = ImageTk.PhotoImage(Image.open("assets/o_2.png"))
-square_photo = ImageTk.PhotoImage(Image.open("assets/square.png"))
-xButton = Button(canv,image = x_Photo, command = player_x())
-oButton = Button(canv,image = o_Photo, command = player_o())
-squareButton = Button(canv,image = square_photo, command = player_o())
-
+def buttonClicked(x, y, button):
+    #print('clicked', y , x)
+    global nClick, reset
+    nClick += 1
+    if nClick % 2 == 0:
+        button.configure(image = o_Photo)
+        o_clicked[3*y + x] = 1
+    else :
+        button.configure(image = x_Photo)
+        x_clicked[3*y + x] = 1
+    if check_score(o_clicked):
+        print(' O won!')
+        messagebox.showinfo("Score", " O won!") 
+        reset = True
+    if check_score(x_clicked):
+        print(' X won!')
+        messagebox.showinfo("Score", " X won!") 
+        reset = True
 class CanvasButton:
     def __init__(self, canvas, x, y):
+        pivots = [ 58 , 210 , 350]
         self.canvas = canvas
+        self.x = x
+        self.y = y
         self.button = Button(canvas, image = square_photo,
-                                command=self.buttonclicked)
-        self.id = canvas.create_window(x, y, width=STEP, height=STEP,
+                                command= lambda:buttonClicked(x , y, self.button))
+        self.id = canvas.create_window(pivots[x], pivots[y], width=STEP, height=STEP,
                                        window=self.button)
+        self.clicked = False
         self.player = -1
-    def buttonclicked(self):
-        global nClick
-        nClick += 1
-        if nClick % 2 == 0:
-            self.button.configure(image = x_Photo)
-            self.player = 1
-        else :
-            self.button.configure(image = o_Photo)
-            self.player = 0
+        
 
 
-canv.create_image(210,210, image=backkground)      
-pivots = [ 58 , 210 , 350]
-buttons = []
 def play():
-    for row in pivots:
-        for column in pivots:
-            buttons.append(CanvasButton(canv, row, column))
-    
+    B00 = CanvasButton(canv, 0, 0)
+    B01 = CanvasButton(canv, 0, 1)
+    B02 = CanvasButton(canv, 0, 2)
+    B10 = CanvasButton(canv, 1, 0)
+    B11 = CanvasButton(canv, 1, 1)
+    B12 = CanvasButton(canv, 1, 2)
+    B20 = CanvasButton(canv, 2, 0)
+    B21 = CanvasButton(canv, 2, 1)
+    B22 = CanvasButton(canv, 2, 2)
 
-
-play()
+play()  
 root.mainloop()
+
